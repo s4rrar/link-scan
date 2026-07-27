e11d0f5fb08041513ddfed14aa823804258b716c
s4rrar <hassansarrar5@gmail.com>
2026-07-27 14:26:31 +0300

    07/27/2026

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.linkscan.org"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
